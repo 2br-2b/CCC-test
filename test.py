d3c454732c93f881923d00aeb9556737d336f900
2br-2b <johnwuller+git@gmail.com>
2020-02-06 20:18:40 -0500

    Got the headers working

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import json
 
+def print_keys(data):
+    for key, value in data.items():
+        print("Key:")
+        print(key)
+
+with open('ccc.json', 'rt') as f:
+    ccc = json.load(f)
+
+print(ccc['toc_nodes']['toc-1']['text'])
+print_keys(ccc['page_nodes']['toc-1'])
+
