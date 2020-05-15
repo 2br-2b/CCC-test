6ae0badd42a51cf7fe8d83e58ce6d3b59c7c0f3e
CLASSIFIED <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2020-05-15 13:50:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import json
 
 def print_keys(data):
+    #prints all of the keys in a dictionary
     for key, value in data.items():
         print("Key:")
         print(key)
@@ -12,15 +13,18 @@ s = ''
 
 print_keys(ccc['page_nodes'])
 
+#find all of the text blocks from the CCC
 for i in range(1, 396):
     try:
         string = 'toc-' + str(i)
         for item in ccc['page_nodes'][string]['paragraphs']:
             for item2 in item['elements']:
                 if item2['type'] == 'text':
+                    #if the item is a block of text, add it to the string
                     s += item2['text'] + '\n'
     except:
-        "hi"
+        pass
 
+#Save them all to a file
 with open('ccc.txt', 'w') as f: 
     f.write(s)
